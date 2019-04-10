d4fa91973dead58fbabc518426a172dfaeac7a36
Luiz Felipe F M Costa <luiz@thenets.org>
2019-04-09 23:00:14 -0300

    Added glfw dependency

--- Tetris/src/Main.cpp
+++ Tetris/src/Main.cpp
@@ -1,13 +1,15 @@
 #include "pch.h"
 #include "Tetris.h"
 #include "Screen.h"
-#include "Input.h"
+#include "Input.h"	
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include "GLFW/glfw3.h"
 
 #define DEBUG(x) std::cout << x << std::endl;
 
+#if 1
 int main() {
 	// Main vars
 	Tetris gameState;
@@ -36,7 +38,46 @@ int main() {
 	}
 
 	// Finished
-	DEBUG("FINISHED");
-
-	return 0;
+	DEBUG("FINISHED APP");
 }
+
+#else
+
+int main() {
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	DEBUG("FINISHED SANDBOX");
+}
+
+
+#endif
