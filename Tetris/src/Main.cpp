0d1b3c4d9b1b6471e55eba8fc12eef8269d1c4c4
Luiz Felipe F M Costa <luiz@thenets.org>
2019-04-13 16:27:34 -0300

    Added error handling functions and macros

--- Tetris/src/Main.cpp
+++ Tetris/src/Main.cpp
@@ -15,6 +15,23 @@
 #define RUN_PLAYGROUND 1
 
 
+// OpenGL error handler
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+static void GLClearError() {
+	while (glGetError() != GL_NO_ERROR);
+}
+static bool GLLogCall(const char* function, const char* file, int line) {
+	while (GLenum error = glGetError()) {
+		std::cout << "[OpenGL Error] (" << error << "): " << function <<
+			" " << file << ":" << line << std::endl;
+		return false;
+	}
+	return true;
+}
+
+
+// OpenGL shader functions
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
 	unsigned int id = glCreateShader(type);
 	const char* src = source.c_str();
@@ -90,11 +107,11 @@ int main() {
 		 0.5f,  -0.5f
 	};
 	unsigned int buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	GLCall(glGenBuffers(1, &buffer));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW));
+	GLCall(glEnableVertexAttribArray(0));
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
 
 	std::string vertexShader =
 		"#version 330 core\n"
@@ -115,13 +132,15 @@ int main() {
 		"	color = vec4(1.0, 0.3, 0.3, 1.0);\n"
 		"}\n";
 	unsigned int shader = CreateShader(vertexShader, fragmentShader);
-	glUseProgram(shader);
+	GLCall(glUseProgram(shader));
 
 	// Loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
